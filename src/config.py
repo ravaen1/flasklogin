39d934e29f066b05b329ce98fdda1e4d801ef349
rai <ravaen1001@gmail.com>
2023-04-06 21:24:30 -0500

    revisin del config

--- src/config.py
+++ src/config.py
@@ -1,6 +1,15 @@
-class DevelopmentConfig():
+class Config:
+    SECRET_KEY = 'B!1w8NAt1T^%kvhUI*S^'
+
+
+class DevelopmentConfig(Config):
     DEBUG = True
+    MYSQL_HOST = 'localhost'
+    MYSQL_USER = 'root'
+    MYSQL_PASSWORD = ''
+    MYSQL_DB = 'flask_login'
+
 
 config = {
     'development': DevelopmentConfig
- }
+}
